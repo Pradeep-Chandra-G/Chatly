16dc02b7b4d9d10fed85a23f7bb2ce4bcc773213
Pradeep-Chandra-17 <pradeepchandragajendra@gmail.com>
2025-12-28 19:45:00 +0530

    Removed output: standalone in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 const nextConfig = {
-  output: 'standalone',
   images: {
     unoptimized: true,
   },
   experimental: {
     // Remove if not using Server Components
-    serverComponentsExternalPackages: ['mongodb'],
+    serverComponentsExternalPackages: ["mongodb"],
   },
   webpack(config, { dev }) {
     if (dev) {
@@ -13,7 +12,7 @@ const nextConfig = {
       config.watchOptions = {
         poll: 2000, // check every 2 seconds
         aggregateTimeout: 300, // wait before rebuilding
-        ignored: ['**/node_modules'],
+        ignored: ["**/node_modules"],
       };
     }
     return config;
@@ -29,8 +28,14 @@ const nextConfig = {
         headers: [
           { key: "X-Frame-Options", value: "ALLOWALL" },
           { key: "Content-Security-Policy", value: "frame-ancestors *;" },
-          { key: "Access-Control-Allow-Origin", value: process.env.CORS_ORIGINS || "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: process.env.CORS_ORIGINS || "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
           { key: "Access-Control-Allow-Headers", value: "*" },
         ],
       },
